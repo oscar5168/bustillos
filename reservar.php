45080e639cad37aab8bb95cae1c30cf447b4cbf0
oscar5168 <150090746+oscar5168@users.noreply.github.com>
2025-07-13 19:11:23 -0600

    subbiendo mejoras

--- reservar.php
+++ reservar.php
@@ -10,8 +10,34 @@ $conexion = new mysqli($host,$user,$pasword,$bd);
 
 if($conexion ->connect_error){
     die("Conexion fallida". $conexion->connect_error);
-}else{
-    echo("Conexion exitosa");
 }
+
+$personas = $_POST['personas'];
+$fecha = $_POST['fecha'];
+$hora_inicio = $_POST['hora_inicio'];
+$hora_fin = $_POST['hora_fin'];
+
+
+
+
+$ask = "SELECT * FROM reserva
+        WHERE fecha = '$fecha'
+        AND NOT (
+            '$hora_fin' <= hora_entrada OR
+            '$hora_inicio' >= hora_salida
+        )";
+
+$insert = "INSERT INTO reserva(ID, personas, fecha, hora_entrada, hora_salida) VALUES('NULL', '$personas', '$fecha', '$hora_inicio', '$hora_fin')";
+
+$result = $conexion->query($ask);
+
+if($result->num_rows == 0){
+    $conexion->query($insert);
+    echo "Reserva exitosa";
+}else{
+    echo "Reserva no disponible";
+}
+
+
 
 ?>
